23034b0b872a86236f2c305b948f879ec1bbcdbe
viniferp <augustfp18@gmail.com>
2021-12-22 11:44:25 -0300

    Studying NodeJs fixing exercise problems. Solved!

--- src/index.js
+++ src/index.js
@@ -23,22 +23,32 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repository = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  if(title) {
+    repository.title = title;
+  }
 
-  repositories[repositoryIndex] = repository;
+  if(url) {
+    repository.url = url;
+  }
+
+  if(techs) {
+    repository.techs = techs;
+  }
 
   return response.json(repository);
 });
@@ -48,7 +58,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -68,7 +78,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
